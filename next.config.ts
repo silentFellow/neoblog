ce4ef5d1e8c36807f380a3adb6f4352e36858077
silentFellow <gowtham.sri+github@zohomail.in>
2025-02-09 17:43:29 +0530

    Update next.config.ts to be compatible with uploadthing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "4mb",
+    },
+  },
 };
 
 export default nextConfig;
